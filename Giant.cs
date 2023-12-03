d844d279e6561e211032bdfe20c61f29efd5c9c9
KockAnigilator <118073863+KockAnigilator@users.noreply.github.com>
2023-12-03 15:32:44 +0300

    Гигант

---
+++ Giant.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+namespace DubstepFight
+{
+    public class Giant : Races
+    {
+
+
+
+        public Giant()
+        {
+            Health = 200;
+            Armor = 20;
+            Speed = 10;
+        }
+
+        public double Attack()
+        {
+            return (Health * 0.01) + 8;
+        }
+
+
+    }
+}
